45a6945a1214be37970a8f26bc2a1c88a3ff8e0f
Marcelo Pottier <marcelopottier@gmail.com>
2025-10-28 16:59:56 -0300

    novos servicos injetados

---
+++ events-service/src/main/java/com/example/eventsservice/EventsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsserviceApplication.class, args);
+	}
+
+}
